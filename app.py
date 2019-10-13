7ce00fa76e4660f109a995d32808839703800e37
Willa <willa.chen@vudu.com>
2019-10-13 14:10:13 -0700

    added sender_action

--- app.py
+++ app.py
@@ -36,10 +36,19 @@ def webhook():
 
                 if messaging_event.get("message"):  # someone sent us a message
 
+                    # TODO: validate payload
+                    # Generate a SHA1 signature using the payload and your app's App Secret.
+                    # Compare your signature to the signature in the X-Hub-Signature header (everything after sha1=). If the signatures match, the payload is genuine.
+
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
+                    if messaging_event["message"].get("quick_reply"):  # they sent a quick reply
+                        message_body = messaging_event["message"]["quick_reply"]["payload"]
+                        message_type = "quick_reply"
+
+                    sender_action(sender_id)
                     send_message(sender_id, "roger that!")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
@@ -55,6 +64,7 @@ def webhook():
 
 
 def send_message(recipient_id, message_text):
+    # Facebook's Send API reference: https://developers.facebook.com/docs/messenger-platform/reference/send-api/
 
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
@@ -70,6 +80,32 @@ def send_message(recipient_id, message_text):
         },
         "message": {
             "text": message_text
+        },
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def sender_action(recipient_id):
+    # Facebook's Send API reference: https://developers.facebook.com/docs/messenger-platform/reference/send-api/
+
+    log("activating sender_action to {recipient}".format(recipient=recipient_id))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "sender_action": {
+            "typing_on": True,
+            "mark_seen": True
         }
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)

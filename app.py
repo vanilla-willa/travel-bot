37462dc7968ab47259a407e114fc637288234017
Willa <willa.chen@vudu.com>
2019-10-16 03:52:26 -0700

    revert

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from datetime import datetime
-from brain import Brain
+# from brain import Brain
 from brain import process_message
 from flask import Flask, request
 import requests

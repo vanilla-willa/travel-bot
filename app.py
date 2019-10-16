0149193c01778bea495c60c5f209e8f8c207e8e6
Willa <willa.chen@vudu.com>
2019-10-16 03:47:31 -0700

    revert class changes

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import sys
 from datetime import datetime
 from brain import Brain
 from flask import Flask, request
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -39,22 +41,27 @@ def webhook():
                     # Compare your signature to the signature in the X-Hub-Signature header (everything after sha1=).
                     # If the signatures match, the payload is genuine.
 
-                    user_id = messaging_event["sender"]["id"]        # user's facebook ID
+                    sender_id = messaging_event["sender"]["id"]        # user's facebook ID
                     recipient_id = messaging_event["recipient"]["id"]  # your page's facebook ID
 
-                    decision = Brain()
+                    if messaging_event["message"].get("text"):  # user sent a text message
+                        message = messaging_event["message"].get("text")
+                        message_type = "text"
 
-                    decision.mark_message_read(user_id)
-                    decision.response_in_progress(user_id)
+                    elif messaging_event["message"].get("quick_reply"):  # user sent a quick reply
+                        # message_payload will be the same as the text
+                        message = messaging_event["message"]["quick_reply"]["payload"]
+                        message_type = "quick_reply"
 
-                    message_type = decision.determine_message_type(user_id, messaging_event)
-                    if message_type is None:
-                        return "ok", 200
+                    else:
+                        send_message(sender_id, dict(text="Sorry. I currently do not support anything beyond "
+                                                          "text and quick reply"))
 
-                    message = decision.read_message_text(user_id, messaging_event)
+                    mark_message_read(sender_id)
+                    response_in_progress(sender_id)
 
-                    msg_data = decision.process_message(message_type, message)
-                    decision.send_message(user_id, msg_data)
+                    msg_data = process_message(message_type, message)
+                    send_message(sender_id, msg_data)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -63,6 +70,90 @@ def webhook():
                     pass
 
     return "ok", 200
+
+
+def send_message(recipient_id, message):
+    # Facebook's Send API reference: https://developers.facebook.com/docs/messenger-platform/reference/send-api/
+    # message parameter will contain both text and quick response
+    bot_text = message.get("text")
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=bot_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": message
+        # {
+        #     "text": message,
+        #     "quick_replies": [{
+        #         "content_type": "text",
+        #         "title": "test1",
+        #         "payload": "test1"
+        #     }, {
+        #         "content_type": "text",
+        #         "title": "test2",
+        #         "payload": "test2"
+        #       }
+        #     ]
+        # }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def mark_message_read(recipient_id):
+    # Facebook's Send API reference: https://developers.facebook.com/docs/messenger-platform/reference/send-api/
+
+    log("Marking message as read")
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "sender_action": "mark_seen"
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def response_in_progress(recipient_id):
+    # Facebook's Send API reference: https://developers.facebook.com/docs/messenger-platform/reference/send-api/
+
+    log("Sending ... chat bubble to user")
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "sender_action": "typing_on"
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
 
 
 def log(message):  # simple wrapper for logging to stdout on heroku
--- brain.py
+++ brain.py
@@ -5,131 +5,158 @@ from datetime import datetime
 import requests
 import json
 
-class Brain:
 
-    def __init__(self):
-        pass
+# class Brain:
+#
+#     def __init__(self):
+#         pass
+#
+#     def determine_message_type(self, user_id, messaging_event):
+#         if messaging_event["message"].get("text"):  # user sent a text message
+#             return "text"
+#
+#         elif messaging_event["message"].get("quick_reply"):  # user sent a quick reply
+#             return "quick_reply"
+#
+#         else:
+#             self.send_message(user_id, dict(text="Sorry. I currently do not support anything beyond "
+#                                                  "text and quick reply"))
+#             return None
+#
+#     def read_message_text(self, user_id, messaging_event):
+#         if self.determine_message_type(user_id, messaging_event) == "text":
+#             return messaging_event["message"].get("text")
+#         else:
+#             return messaging_event["message"]["quick_reply"]["payload"]
+#
+#     def process_message(self, message_type, message):
+#         """
+#         User input of either text or quick reply returns additional text and quick reply
+#         Payload from quick reply lets you know which button was clicked
+#         :param message: either text or payload
+#         :param message_type: either text or quick_reply
+#         :return: dict with text and quick_reply
+#         quick_reply is a list of dicts
+#         """
+#
+#         response = {}
+#
+#         if message in consts.GREETINGS:
+#             self.log("Updated to response dict. Currently looks like: {}".format(response))
+#             quick_reply_list = list(dict(content_type="text", title=opt, payload=opt) for opt in consts.INITIAL)
+#             response.update(dict(quick_replies=quick_reply_list))
+#             self.log("Updated to response dict. Currently looks like: {}".format(response))
+#
+#         return response
+#
+#     def send_message(self, user_id, message):
+#         # Facebook's Send API reference: https://developers.facebook.com/docs/messenger-platform/reference/send-api/
+#         # message parameter will contain both text and quick response
+#         bot_text = message.get("text")
+#
+#         self.log("sending message to {recipient}: {text}".format(recipient=user_id, text=bot_text))
+#
+#         params = {
+#             "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+#         }
+#         headers = {
+#             "Content-Type": "application/json"
+#         }
+#         data = json.dumps({
+#             "recipient": {
+#                 "id": user_id
+#             },
+#             "message": message
+#             # {
+#             #     "text": message,
+#             #     "quick_replies": [{
+#             #         "content_type": "text",
+#             #         "title": "test1",
+#             #         "payload": "test1"
+#             #     }, {
+#             #         "content_type": "text",
+#             #         "title": "test2",
+#             #         "payload": "test2"
+#             #       }
+#             #     ]
+#             # }
+#         })
+#         r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+#         if r.status_code != 200:
+#             self.log(r.status_code)
+#             self.log(r.text)
+#
+#     def mark_message_read(self, user_id):
+#         # Facebook's Send API reference: https://developers.facebook.com/docs/messenger-platform/reference/send-api/
+#
+#         self.log("Marking message as read")
+#
+#         params = {
+#             "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+#         }
+#         headers = {
+#             "Content-Type": "application/json"
+#         }
+#         data = json.dumps({
+#             "recipient": {
+#                 "id": user_id
+#             },
+#             "sender_action": "mark_seen"
+#         })
+#         r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+#         if r.status_code != 200:
+#             self.log(r.status_code)
+#             self.log(r.text)
+#
+#     def typing(self, user_id):
+#         # Facebook's Send API reference: https://developers.facebook.com/docs/messenger-platform/reference/send-api/
+#
+#         self.log("Sending ... chat bubble to user")
+#
+#         params = {
+#             "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+#         }
+#         headers = {
+#             "Content-Type": "application/json"
+#         }
+#         data = json.dumps({
+#             "recipient": {
+#                 "id": user_id
+#             },
+#             "sender_action": "typing_on"
+#         })
+#         r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+#         if r.status_code != 200:
+#             self.log(r.status_code)
+#             self.log(r.text)
+#
+#     @staticmethod
+#     def log(message):
+#         print("=== {} DEBUG MSG:: {} ===".format(datetime.now(), message))
+#         sys.stdout.flush()
 
-    def determine_message_type(self, user_id, messaging_event):
-        if messaging_event["message"].get("text"):  # user sent a text message
-            return "text"
+def process_message(message_type, message):
+    """
+    User input of either text or quick reply returns additional text and quick reply
+    Payload from quick reply lets you know which button was clicked
+    :param message: either text or payload
+    :param message_type: either text or quick_reply
+    :return: dict with text and quick_reply
+    quick_reply is a list of dicts
+    """
 
-        elif messaging_event["message"].get("quick_reply"):  # user sent a quick reply
-            return "quick_reply"
+    response = {}
 
-        else:
-            self.send_message(user_id, dict(text="Sorry. I currently do not support anything beyond "
-                                                 "text and quick reply"))
-            return None
+    if message in consts.GREETINGS:
+        response.update(dict(text=consts.TEXT_OUTPUTS["start"]))
+        log("Updated to response dict. Currently looks like: {}".format(response))
+        quick_reply_list = list(dict(content_type="text", title=opt, payload=opt) for opt in consts.INITIAL)
+        response.update(dict(quick_replies=quick_reply_list))
+        log("Updated to response dict. Currently looks like: {}".format(response))
 
-    def read_message_text(self, user_id, messaging_event):
-        if self.determine_message_type(user_id, messaging_event) == "text":
-            return messaging_event["message"].get("text")
-        else:
-            return messaging_event["message"]["quick_reply"]["payload"]
+    return response
 
-    def process_message(self, message_type, message):
-        """
-        User input of either text or quick reply returns additional text and quick reply
-        Payload from quick reply lets you know which button was clicked
-        :param message: either text or payload
-        :param message_type: either text or quick_reply
-        :return: dict with text and quick_reply
-        quick_reply is a list of dicts
-        """
 
-        response = {}
-
-        if message in consts.GREETINGS:
-            self.log("Updated to response dict. Currently looks like: {}".format(response))
-            quick_reply_list = list(dict(content_type="text", title=opt, payload=opt) for opt in consts.INITIAL)
-            response.update(dict(quick_replies=quick_reply_list))
-            self.log("Updated to response dict. Currently looks like: {}".format(response))
-
-        return response
-
-    def send_message(self, user_id, message):
-        # Facebook's Send API reference: https://developers.facebook.com/docs/messenger-platform/reference/send-api/
-        # message parameter will contain both text and quick response
-        bot_text = message.get("text")
-
-        self.log("sending message to {recipient}: {text}".format(recipient=user_id, text=bot_text))
-
-        params = {
-            "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-        }
-        headers = {
-            "Content-Type": "application/json"
-        }
-        data = json.dumps({
-            "recipient": {
-                "id": user_id
-            },
-            "message": message
-            # {
-            #     "text": message,
-            #     "quick_replies": [{
-            #         "content_type": "text",
-            #         "title": "test1",
-            #         "payload": "test1"
-            #     }, {
-            #         "content_type": "text",
-            #         "title": "test2",
-            #         "payload": "test2"
-            #       }
-            #     ]
-            # }
-        })
-        r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-        if r.status_code != 200:
-            self.log(r.status_code)
-            self.log(r.text)
-
-    def mark_message_read(self, user_id):
-        # Facebook's Send API reference: https://developers.facebook.com/docs/messenger-platform/reference/send-api/
-
-        self.log("Marking message as read")
-
-        params = {
-            "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-        }
-        headers = {
-            "Content-Type": "application/json"
-        }
-        data = json.dumps({
-            "recipient": {
-                "id": user_id
-            },
-            "sender_action": "mark_seen"
-        })
-        r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-        if r.status_code != 200:
-            self.log(r.status_code)
-            self.log(r.text)
-
-    def response_in_progress(self, user_id):
-        # Facebook's Send API reference: https://developers.facebook.com/docs/messenger-platform/reference/send-api/
-
-        self.log("Sending ... chat bubble to user")
-
-        params = {
-            "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-        }
-        headers = {
-            "Content-Type": "application/json"
-        }
-        data = json.dumps({
-            "recipient": {
-                "id": user_id
-            },
-            "sender_action": "typing_on"
-        })
-        r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-        if r.status_code != 200:
-            self.log(r.status_code)
-            self.log(r.text)
-
-    @staticmethod
-    def log(message):  # simple wrapper for logging to stdout on heroku
-        print("=== {} DEBUG MSG:: {} ===".format(datetime.now(), message))
-        sys.stdout.flush()
+def log(message):
+    print("=== {} DEBUG MSG:: {} ===".format(datetime.now(), message))
+    sys.stdout.flush()

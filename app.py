9754406d207f66a7c75c09c1152fc6b244140956
Willa <willa.chen@vudu.com>
2019-10-18 01:35:05 -0700

    debug

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ def webhook():
                     #                                              "text and quick reply"))
                     #     return "ok", 200
 
+                    print("!!!!!! DEBUG: ", messaging_event["message"].get("quick_reply"))
                     message_properties = decision.read_message_text(messaging_event)
                     if message_properties is None:
                         decision.send_message(user_id, dict(text="Sorry. I currently do not support anything beyond "
